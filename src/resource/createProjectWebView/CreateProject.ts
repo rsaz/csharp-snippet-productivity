a60235eb171d32765986bb5cbf45aa16100c9924
Richard Zampieri <br.developer@gmail.com>
2024-01-13 03:36:38 -0800

    improve terminal cmd execution and framework compatibility

--- src/resource/createProjectWebView/CreateProject.ts
+++ src/resource/createProjectWebView/CreateProject.ts
@@ -3,380 +3,210 @@ import * as path from "path";
 import * as vscode from "vscode";
 import { getTargetFrameworks } from "../../utils/sdk.provider";
 import { getNonce } from "./GetNonce";
+import { CommandFactory, Message } from "../../utils/terminal-cmd.provider";
 
 export class CreateProjectPanel {
-  private static context: vscode.ExtensionContext;
-  private static _filepath: any = "";
-  private static _panel: vscode.WebviewPanel;
-  private static _disposables: vscode.Disposable[] = [];
-  private static _sdks: string[] = [];
-  private static _defaultFolder: vscode.WorkspaceConfiguration | undefined;
-  private static _terminal: vscode.Terminal;
+    private static context: vscode.ExtensionContext;
+    private static _filepath: any = "";
+    private static _panel: vscode.WebviewPanel;
+    private static _disposables: vscode.Disposable[] = [];
+    private static _sdks: string[] = [];
+    private static _defaultFolder: vscode.WorkspaceConfiguration | undefined;
+    private static _terminal: vscode.Terminal;
 
-  // To avoid direct instantiation use the createOrShow method
-  private constructor() {}
+    // To avoid direct instantiation use the createOrShow method
+    private constructor() {}
 
-  // Main method to create or show the panel
-  public static createOrShow(context: vscode.ExtensionContext): void {
-    this.context = context;
+    // Main method to create or show the panel
+    public static createOrShow(context: vscode.ExtensionContext): void {
+        this.context = context;
 
-    const column = vscode.window.activeTextEditor
-      ? vscode.window.activeTextEditor.viewColumn
-      : undefined;
+        const column = vscode.window.activeTextEditor
+            ? vscode.window.activeTextEditor.viewColumn
+            : undefined;
 
-    // If we already have a panel, show it.
-    if (CreateProjectPanel._panel) {
-      CreateProjectPanel._panel.reveal(column);
-      CreateProjectPanel.update();
-      return;
-    }
-
-    // Otherwise, create a new panel.
-    const panel = vscode.window.createWebviewPanel(
-      "create-project",
-      "Create Project",
-      column || vscode.ViewColumn.One,
-      {
-        enableScripts: true,
-        localResourceRoots: [
-          vscode.Uri.joinPath(context.extensionUri, "media"),
-          vscode.Uri.joinPath(context.extensionUri, "out/compiled"),
-        ],
-      }
-    );
-
-    // adding panel icon
-    panel.iconPath = vscode.Uri.file(
-      path.join(this.context.extensionPath, "media", "addProjectIcon.png")
-    );
-
-    CreateProjectPanel.defaultConstructor(panel);
-  }
-
-  private static async defaultConstructor(panel: vscode.WebviewPanel) {
-    this._panel = panel;
-
-    this._defaultFolder = vscode.workspace
-      .getConfiguration("csharp-snippet-productivity")
-      .get("defaultFolderForProjectCreation");
-
-    if (!this._defaultFolder) {
-      vscode.window.showInformationMessage("Please set a default folder for project creation");
-    }
-
-    this._filepath = this._defaultFolder;
-    this._terminal =
-      vscode.window.activeTerminal === undefined
-        ? vscode.window.createTerminal()
-        : vscode.window.activeTerminal;
-    this._terminal.show();
-
-    // OnPanel Close
-    this._panel.onDidDispose(() => this.dispose(), null, this._disposables);
-
-    this._panel.webview.onDidReceiveMessage(
-      async (message) => {
-        switch (message.command) {
-          case "createProject": //console
-            await this.projectCreation(message);
-            return;
-
-          case "selectDirectory":
-            const options: vscode.OpenDialogOptions = {
-              canSelectMany: false,
-              openLabel: "Select",
-              canSelectFiles: false,
-              canSelectFolders: true,
-            };
-
-            vscode.window.showOpenDialog(options).then((fileUri) => {
-              if (fileUri && fileUri[0]) {
-                this._filepath = fileUri[0].fsPath;
-                this.update(
-                  message.templateName,
-                  message.template,
-                  message.project,
-                  message.solution,
-                  message.framework
-                );
-              }
-            });
+        // If we already have a panel, show it.
+        if (CreateProjectPanel._panel) {
+            CreateProjectPanel._panel.reveal(column);
+            CreateProjectPanel.update();
             return;
         }
-      },
-      null,
-      this._disposables
-    );
 
-    // Set the Webview initial html content
-    this.update();
-  }
+        // Otherwise, create a new panel.
+        const panel = vscode.window.createWebviewPanel(
+            "create-project",
+            "Create Project",
+            column || vscode.ViewColumn.One,
+            {
+                enableScripts: true,
+                localResourceRoots: [
+                    vscode.Uri.joinPath(context.extensionUri, "media"),
+                    vscode.Uri.joinPath(context.extensionUri, "out/compiled"),
+                ],
+            }
+        );
 
-  private static dispose() {
-    // Clean up our resources
-    this._panel.dispose();
+        // adding panel icon
+        panel.iconPath = vscode.Uri.file(
+            path.join(this.context.extensionPath, "media", "addProjectIcon.png")
+        );
 
-    CreateProjectPanel._panel = undefined as any;
-
-    while (this._disposables.length) {
-      const x = this._disposables.pop();
-      if (x) {
-        x.dispose();
-      }
-    }
-  }
-
-  private static async projectCreation(message: any) {
-    if (fs.existsSync(this._filepath + "\\" + message.solution)) {
-      vscode.window.showErrorMessage("Solution folder already exist");
-      return;
+        CreateProjectPanel.defaultConstructor(panel);
     }
 
-    if (message.template === "grpc") {
-      this._terminal.sendText("mkdir " + "'" + this._filepath + "\\" + message.solution + "'");
-      this._terminal.sendText(
-        "dotnet new sln -n " +
-          message.solution +
-          " -o " +
-          "'" +
-          this._filepath +
-          "\\" +
-          message.solution +
-          "'" +
-          " --force"
-      );
-      this._terminal.sendText(
-        "mkdir " + "'" + this._filepath + "\\" + message.solution + "\\" + message.project + "'"
-      );
-      this._terminal.sendText(
-        "dotnet new " +
-          message.template +
-          " --language c# -n " +
-          message.project +
-          " -o " +
-          "'" +
-          this._filepath +
-          "\\" +
-          message.solution +
-          "\\" +
-          message.project +
-          "'" +
-          " --force"
-      );
-      this._terminal.sendText(
-        "dotnet sln " +
-          "'" +
-          this._filepath +
-          "\\" +
-          message.solution +
-          "\\" +
-          message.solution +
-          ".sln" +
-          "'" +
-          " add " +
-          "'" +
-          this._filepath +
-          "\\" +
-          message.solution +
-          "\\" +
-          message.project +
-          "\\" +
-          message.project +
-          ".csproj" +
-          "'"
-      );
-      this._terminal.sendText(
-        "code " + "'" + this._filepath + "\\" + message.solution + "'" + " -r"
-      );
-    } else if (message.template === "minwebapi") {
-      if (message.framework !== "net6.0") {
-        vscode.window.showWarningMessage("Please select net6.0 for Minimal WebAPI");
-        return;
-      }
+    private static async defaultConstructor(panel: vscode.WebviewPanel) {
+        this._panel = panel;
 
-      this._terminal.sendText("mkdir " + "'" + this._filepath + "\\" + message.solution + "'");
-      this._terminal.sendText(
-        "dotnet new sln -n " +
-          message.solution +
-          " -o " +
-          "'" +
-          this._filepath +
-          "\\" +
-          message.solution +
-          "'" +
-          " --force"
-      );
-      this._terminal.sendText(
-        "mkdir " + "'" + this._filepath + "\\" + message.solution + "\\" + message.project + "'"
-      );
-      this._terminal.sendText(
-        "dotnet new webapi -minimal --language c# -n " +
-          message.project +
-          " -o " +
-          "'" +
-          this._filepath +
-          "\\" +
-          message.solution +
-          "\\" +
-          message.project +
-          "'" +
-          " --framework " +
-          message.framework +
-          " --force"
-      );
-      this._terminal.sendText(
-        "dotnet sln " +
-          "'" +
-          this._filepath +
-          "\\" +
-          message.solution +
-          "\\" +
-          message.solution +
-          ".sln" +
-          "'" +
-          " add " +
-          "'" +
-          this._filepath +
-          "\\" +
-          message.solution +
-          "\\" +
-          message.project +
-          "\\" +
-          message.project +
-          ".csproj" +
-          "'"
-      );
-      this._terminal.sendText(
-        "code " + "'" + this._filepath + "\\" + message.solution + "'" + " -r"
-      );
-    } else {
-      this._terminal.sendText("mkdir " + "'" + this._filepath + "\\" + message.solution + "'");
-      this._terminal.sendText(
-        "dotnet new sln -n " +
-          message.solution +
-          " -o " +
-          "'" +
-          this._filepath +
-          "\\" +
-          message.solution +
-          "'" +
-          " --force"
-      );
-      this._terminal.sendText(
-        "mkdir " + "'" + this._filepath + "\\" + message.solution + "\\" + message.project + "'"
-      );
-      this._terminal.sendText(
-        "dotnet new " +
-          message.template +
-          " --language c# -n " +
-          message.project +
-          " -o " +
-          "'" +
-          this._filepath +
-          "\\" +
-          message.solution +
-          "\\" +
-          message.project +
-          "'" +
-          " --framework " +
-          message.framework +
-          " --force"
-      );
-      this._terminal.sendText(
-        "dotnet sln " +
-          "'" +
-          this._filepath +
-          "\\" +
-          message.solution +
-          "\\" +
-          message.solution +
-          ".sln" +
-          "'" +
-          " add " +
-          "'" +
-          this._filepath +
-          "\\" +
-          message.solution +
-          "\\" +
-          message.project +
-          "\\" +
-          message.project +
-          ".csproj" +
-          "'"
-      );
-      this._terminal.sendText(
-        "code " + "'" + this._filepath + "\\" + message.solution + "'" + " -r"
-      );
+        this._defaultFolder = vscode.workspace
+            .getConfiguration("csharp-snippet-productivity")
+            .get("defaultFolderForProjectCreation");
+
+        if (!this._defaultFolder) {
+            vscode.window.showInformationMessage(
+                "Please set a default folder for project creation"
+            );
+        }
+
+        this._filepath = this._defaultFolder;
+        this._terminal =
+            vscode.window.activeTerminal === undefined
+                ? vscode.window.createTerminal()
+                : vscode.window.activeTerminal;
+        this._terminal.show();
+
+        // OnPanel Close
+        this._panel.onDidDispose(() => this.dispose(), null, this._disposables);
+
+        this._panel.webview.onDidReceiveMessage(
+            async (message) => {
+                switch (message.command) {
+                    case "createProject": //console
+                        await this.projectCreation(message);
+                        return;
+
+                    case "selectDirectory":
+                        const options: vscode.OpenDialogOptions = {
+                            canSelectMany: false,
+                            openLabel: "Select",
+                            canSelectFiles: false,
+                            canSelectFolders: true,
+                        };
+
+                        vscode.window.showOpenDialog(options).then((fileUri) => {
+                            if (fileUri && fileUri[0]) {
+                                this._filepath = fileUri[0].fsPath;
+                                this.update(
+                                    message.templateName,
+                                    message.template,
+                                    message.project,
+                                    message.solution,
+                                    message.framework
+                                );
+                            }
+                        });
+                        return;
+                }
+            },
+            null,
+            this._disposables
+        );
+
+        // Set the Webview initial html content
+        this.update();
     }
-    // setting the current project framework to define the template namespace to be used
-    CreateProjectPanel.context.globalState.update("framework", message.framework);
-  }
 
-  private static async update(
-    projectGroup: any = "api",
-    projectGroupName: any = "Select Project Type",
-    templateName: any = "Select Template",
-    template: any = "console",
-    project: any = "",
-    solution: any = "",
-    framework: any = ""
-  ) {
-    const webview = this._panel.webview;
+    private static dispose() {
+        // Clean up our resources
+        this._panel.dispose();
 
-    // list of sdk's
-    const sdksResource: vscode.Uri = webview.asWebviewUri(
-      vscode.Uri.joinPath(this.context.extensionUri, "media", "sdks.txt")
-    );
-    this._sdks = getTargetFrameworks(sdksResource);
+        CreateProjectPanel._panel = undefined as any;
 
-    this._panel.webview.html = this.getHtmlForWebview(
-      webview,
-      projectGroup,
-      projectGroupName,
-      templateName,
-      template,
-      project,
-      solution,
-      framework
-    );
-  }
+        while (this._disposables.length) {
+            const x = this._disposables.pop();
+            if (x) {
+                x.dispose();
+            }
+        }
+    }
 
-  private static getHtmlForWebview(
-    webview: vscode.Webview,
-    projectGroup: any,
-    projectGroupName: any,
-    templateName: any,
-    template: any,
-    project: any,
-    solution: any,
-    framework: any
-  ) {
-    // main script integration
-    const scriptUri = webview.asWebviewUri(
-      vscode.Uri.joinPath(this.context.extensionUri, "media", "main.js")
-    );
+    private static async projectCreation(message: Message) {
+        if (fs.existsSync(this._filepath + "\\" + message.solution)) {
+            vscode.window.showErrorMessage("Solution folder already exist");
+            return;
+        }
 
-    // Local path to css styles
-    const styleResetPath = vscode.Uri.joinPath(this.context.extensionUri, "media", "reset.css");
-    const stylesPathMainPath = vscode.Uri.joinPath(
-      this.context.extensionUri,
-      "media",
-      "vscode.css"
-    );
+        const command = CommandFactory.getCommand(this._terminal, message);
+        command.execute();
 
-    // Uri to load styles into webview
-    const stylesResetUri = webview.asWebviewUri(styleResetPath);
-    const stylesMainUri = webview.asWebviewUri(stylesPathMainPath);
+        // setting the current project framework to define the template namespace to be used
+        CreateProjectPanel.context.globalState.update("framework", message.framework);
+    }
 
-    // Use a nonce to only allow specific scripts to be run
-    const nonce = getNonce();
+    private static async update(
+        projectGroup: any = "api",
+        projectGroupName: any = "Select Project Type",
+        templateName: any = "Select Template",
+        template: any = "console",
+        project: any = "",
+        solution: any = "",
+        framework: any = ""
+    ) {
+        const webview = this._panel.webview;
 
-    return `<!DOCTYPE html>
+        // list of sdk's
+        const sdksResource: vscode.Uri = webview.asWebviewUri(
+            vscode.Uri.joinPath(this.context.extensionUri, "media", "sdks.txt")
+        );
+        this._sdks = getTargetFrameworks(sdksResource);
+
+        this._panel.webview.html = this.getHtmlForWebview(
+            webview,
+            projectGroup,
+            projectGroupName,
+            templateName,
+            template,
+            project,
+            solution,
+            framework
+        );
+    }
+
+    private static getHtmlForWebview(
+        webview: vscode.Webview,
+        projectGroup: any,
+        projectGroupName: any,
+        templateName: any,
+        template: any,
+        project: any,
+        solution: any,
+        framework: any
+    ) {
+        // main script integration
+        const scriptUri = webview.asWebviewUri(
+            vscode.Uri.joinPath(this.context.extensionUri, "media", "main.js")
+        );
+
+        // Local path to css styles
+        const styleResetPath = vscode.Uri.joinPath(this.context.extensionUri, "media", "reset.css");
+        const stylesPathMainPath = vscode.Uri.joinPath(
+            this.context.extensionUri,
+            "media",
+            "vscode.css"
+        );
+
+        // Uri to load styles into webview
+        const stylesResetUri = webview.asWebviewUri(styleResetPath);
+        const stylesMainUri = webview.asWebviewUri(stylesPathMainPath);
+
+        // Use a nonce to only allow specific scripts to be run
+        const nonce = getNonce();
+
+        return `<!DOCTYPE html>
     <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${
-    webview.cspSource
+      webview.cspSource
   }; img-src ${webview.cspSource} https:; script-src 'nonce-${nonce}';">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="${stylesResetUri}" rel="stylesheet">
@@ -389,8 +219,8 @@ export class CreateProjectPanel {
   <h3>Select the project type</h3>
     <select id="project-group-select" name="project-group">
       <option value="${projectGroup}" selected="selected">${
-      projectGroupName === "" ? "Select Project Type" : projectGroupName
-    }</option>
+            projectGroupName === "" ? "Select Project Type" : projectGroupName
+        }</option>
       <option value="api">API</option>
       <option value="blazor">Blazor</option>
       <option value="cloud">Cloud</option>
@@ -411,8 +241,8 @@ export class CreateProjectPanel {
   <h3>Select the project template</h3>
   <select id="custom-select" name="project-type">
     <option value="${template}" selected="selected">${
-      templateName === "" ? "Select Template" : templateName
-    }</option>
+            templateName === "" ? "Select Template" : templateName
+        }</option>
     <!-- Dynamic templates will be added here -->
   </select>
   </br>
@@ -423,7 +253,7 @@ export class CreateProjectPanel {
   <label for="location">Location:</label>
   <div id="forminline">
     <input id="inputLocal" value="${
-      this._filepath
+        this._filepath
     }" type="text" name="location" required placeholder="Select the location to save your project">
     <button id="selectFolder">...</button>
   </div>
@@ -440,5 +270,5 @@ export class CreateProjectPanel {
   <script nonce="${nonce}" src="${scriptUri}"></script>
   </body>
 </html>`;
-  }
+    }
 }
